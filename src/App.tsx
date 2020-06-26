2f704ebba2855dca4b8b73f8385e15cdf18c8b25
Maxim Nisenblat <nismaxim82@gmail.com>
2020-06-26 14:45:31 +0300

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
-import { createStyles, makeStyles, Theme } from '@material-ui/core';
+import {
+  AppBar,
+  createStyles,
+  IconButton,
+  makeStyles,
+  Theme,
+  Toolbar,
+  Typography
+} from '@material-ui/core';
 import { Provider } from 'mobx-react';
 import React from 'react';
 import classes from './App.module.css';
@@ -7,7 +15,7 @@ import store from './stores';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     body: {
-      padding: theme.spacing(1),
+      padding: theme.spacing(2),
     },
     menuButton: {
       marginLeft: theme.spacing(2),
@@ -20,7 +28,51 @@ function App() {
   return (
     <Provider store={store}>
       <div className={classes.page}>
-        <div className={[classes.body, styles.body].join(' ')}>test</div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.headerTitle}>
+              Внести оплату
+            </Typography>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            />
+          </Toolbar>
+        </AppBar>
+        <AppBar position="static" color="secondary">
+          <Toolbar>
+            <Typography variant="h6" className={classes.headerTitle}>
+              Внести оплату
+            </Typography>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            />
+          </Toolbar>
+        </AppBar>
+        <div className={[classes.body, styles.body].join(' ')}>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+        </div>
       </div>
     </Provider>
   );
