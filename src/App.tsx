72e6aea3c9e4220f6e5c25a15ce829524e780a59
Maxim Nisenblat <nismaxim82@gmail.com>
2020-06-26 14:17:25 +0300

    add mobx, material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
+import { Provider } from 'mobx-react';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
+import store from './stores';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    body: {
+      padding: theme.spacing(1),
+    },
+    menuButton: {
+      marginLeft: theme.spacing(2),
+    },
+  })
+);
 
 function App() {
+  const styles = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className={classes.page}>
+        <div className={[classes.body, styles.body].join(' ')}>test</div>
+      </div>
+    </Provider>
   );
 }
 
