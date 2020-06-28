ba13e458c2c51f3e7ff979017f7421e82813f2eb
Maxim Nisenblat <P0024891@ness-tech.co.il>
2020-06-28 19:10:33 +0300

    Updates

---
+++ precache-manifest.2814b67eccc44131703e30dc059fa302.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89461798e364f10f6cf026998c3d2b10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f48811e252fa132ade60",
+    "url": "/static/css/main.422c8778.chunk.css"
+  },
+  {
+    "revision": "5b936a72d970972b71d7",
+    "url": "/static/js/2.7a11bdc0.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.7a11bdc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f48811e252fa132ade60",
+    "url": "/static/js/main.a3131451.chunk.js"
+  },
+  {
+    "revision": "58d40c288f15a18a7d93",
+    "url": "/static/js/runtime-main.2ef38984.js"
+  }
+]);
